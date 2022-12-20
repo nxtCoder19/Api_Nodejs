f9fee7605350cb82aed6a9f707b6bb94e8888479
piyush <piyush.acet@gmail.com>
2022-12-20 13:08:34 +0530

    done

---
+++ server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { APP_PORT, DB_URL } from "./config";
+import errorHandler from "./middlewares/errorHandler";
+const app = express();
+import routes from "./routes";
+import mongoose from "mongoose";
+import path from "path";
+import cors from "cors";
+
+// Database connection
+mongoose.connect(DB_URL, {
+  dbName: "rest-api",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useFindAndModify: false,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("DB connected...");
+});
+
+//global.appRoot = path.resolve(__dirname);
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/api", routes);
+app.use("/uploads", express.static("uploads"));
+app.use("/", (req, res) => {
+  res.send(`
+  <h1>Welcome to E-commerce Rest APIs</h1>
+  You may contact me <a href="https://codersgyan.com/links/">here</a>
+  Or You may reach out to me for any question related to this Apis: codersgyan@gmail.com
+  `);
+});
+
+app.use(errorHandler);
+const PORT = process.env.PORT || APP_PORT;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
+
+// import express from "express";
+// import { APP_PORT } from "./config";
+
+// const app = express();
+
+// app.get("/", function (req, res) {
+//   res.send("<b>My</b> first express http server");
+// });
+
+// app.listen(APP_PORT, () => console.log(`Listening on port ${APP_PORT}.`));
